49491e8e716838e84a6151a539d1eef094353792
a1 <a1@DESKTOP-JOPBFDP>
2023-11-02 02:23:03 -0700

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int number1 = 0;
+int number2 = 1;
+
+Console.WriteLine("chandomin donbale ro mikhi ?");
+int an = Convert.ToInt32(Console.ReadLine());
+
+for(int x=0; x< an; x=x+2)
+{
+    number1 = number1 + number2;
+    Console.Write(number1);
+    Console.Write(",");
+
+    number2 = number2 + number1;
+    Console.Write(number2);
+    Console.Write(",");
+}
+
+
+
+
+
